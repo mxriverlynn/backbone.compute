d0d3682847b91ccedcddf6e6230f3590076e11e2
Derick Bailey <derickbailey@gmail.com>
2012-09-23 14:22:07 -0500

    spec for calling computed field directly, as a function

--- spec/javascripts/computedField.spec.js
+++ spec/javascripts/computedField.spec.js
@@ -1,4 +1,4 @@
-describe("computed field", function(){
+describe("computed fields", function(){
 
   describe("when computation is done for a single field", function(){
     var model, fieldList;
@@ -57,6 +57,34 @@ describe("computed field", function(){
 
     it("should `set` the computed field on the model to the returned value", function(){
       expect(model.get("computedField")).toEqual("foo-bar");
+    });
+  });
+
+  describe("when calling the computed field attribute as a method", function(){
+    var result;
+
+    var Model = Backbone.Model.extend({
+      initialize: function(){
+        Backbone.Compute(this);
+      },
+
+      computedField: Backbone.Compute("computedField", ["f1", "f2"], function(fields){
+        fieldList = fields;
+        return fields.f1 + "-" + fields.f2
+      })
+    });
+
+    beforeEach(function(){
+      var model = new Model({
+        f1: "foo",
+        f2: "bar"
+      });
+      
+      result = model.computedField();
+    });
+
+    it("return the computed value for the current values of the dependent attributes", function(){
+      expect(result).toEqual("foo-bar");
     });
   });
 
